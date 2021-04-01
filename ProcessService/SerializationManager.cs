f8aaf9282dd5aed37c78e4eab9f89476e40ff068
Alexander Yuskov <a.yuskov@g.nsu.ru>
2021-04-01 20:08:12 +0700

    Add serialization manager

---
+++ ProcessService/SerializationManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace ProcessService {
+	internal class SerializationManager {
+		private readonly TextWriter _writer;
+
+		public SerializationManager(TextWriter writer) {
+			_writer = writer;
+		}
+
+		public void SendProcesses(IEnumerable<Process> processes) {
+			var forSerialization = processes.Select(proc => new ProcessDto { Name = proc.ProcessName }).ToList();
+			var json = JsonSerializer.Serialize(forSerialization);
+			_writer.WriteLine(json);
+			_writer.Flush();
+		}
+
+		private struct ProcessDto {
+			public string Name { get; set; }
+		}
+	}
+}
