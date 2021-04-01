3beba19df23f6fc18a105280261af3a8ee56e62a
Alexander Yuskov <a.yuskov@g.nsu.ru>
2021-04-01 20:08:37 +0700

    Add simple process service realization

--- ProcessService/Program.cs
+++ ProcessService/Program.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace ProcessService {
-	class Program {
-		static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+	internal class Program {
+		static void Main() {
+			var connection = new SerializationManager(Console.Out);
+			while (true) {
+				var processes = Process.GetProcesses();
+				connection.SendProcesses(processes);
+				Thread.Sleep(1000);
+			}
 		}
 	}
 }
