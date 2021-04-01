f66de9952bb28ffc5ffd22886ed611d17aec6b46
Alexander Yuskov <a.yuskov@g.nsu.ru>
2021-04-01 20:45:32 +0700

    Add service manager to gui

---
+++ ProcessGui/ProcessDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessGui {
+	internal struct ProcessDto {
+		public string Name { get; set; }
+	}
+}
---
+++ ProcessGui/ServiceManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace ProcessGui {
+	internal class ServiceManager {
+		public delegate void UpdateProcessesHandler(IEnumerable<ProcessDto> processes);
+		public event UpdateProcessesHandler UpdateProcesses;
+
+		private Process _process;
+
+		public void Start() {
+			var startInfo = new ProcessStartInfo {
+				FileName = "ProcessService.exe",
+				RedirectStandardOutput = true,
+				UseShellExecute = false,
+				CreateNoWindow = true,
+			};
+			_process = Process.Start(startInfo);
+			_process.OutputDataReceived += OnProcessDataRecieved;
+			_process.BeginOutputReadLine();
+		}
+
+		private void OnProcessDataRecieved(object sender, DataReceivedEventArgs e) {
+			if (e.Data == null) return;
+			var processes = JsonSerializer.Deserialize<ProcessDto[]>(e.Data);
+			UpdateProcesses(processes);
+		}
+	}
+}
