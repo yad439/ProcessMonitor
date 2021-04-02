e781dc0762fc5ba412350557e92447b5a14f5df3
Alexander Yuskov <a.yuskov@g.nsu.ru>
2021-04-02 22:14:35 +0700

    Change ProcessDto to reference type

--- ProcessGui/ProcessDto.cs
+++ ProcessGui/ProcessDto.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProcessGui {
-	internal struct ProcessDto {
+﻿namespace ProcessGui {
+	internal class ProcessDto {
 		public string Name { get; set; }
 	}
 }
